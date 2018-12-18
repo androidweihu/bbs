5454a942afac7d1526f35ce14a6e04c319441556
weihu <weihu@jd.com>
2018-12-18 22:00:27 +0800

    点赞功能

--- ormtest.py
+++ ormtest.py
@@ -9,11 +9,4 @@ if __name__ == '__main__':
     from blog import models
     from django.db.models import Count
 
-    # user = models.UserInfo.objects.all()
-    # print(user)
-
-    # user=models.UserInfo.objects.filter(username='weihu').filter()
-    # blog=user.blog
-    # category_list = models.Category.objects.filter(blog=blog).annotate(c=Count("article")).values("title", "c")
-    # print(category_list)
 
